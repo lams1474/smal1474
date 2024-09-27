a2591bb62b127c836d8c58ab58518b244a0422b3
smal1474 <la.maiguas@uea.edu.ec>
2024-09-27 16:57:29 -0500

    Merge remote-tracking branch 'origin/main'

--- Diccionario.py
+++ Diccionario.py
@@ -1,14 +1,30 @@
-# Diccionario llamado 'informacion_personal' con datos ficticios
+# Crear un diccionario llamado 'informacion_personal' con datos ficticios
+# Las claves representan aspectos de la persona: nombre, edad, ciudad y profesión
 informacion_personal = {
-    "nombre": "Luis Maigua",
-    "edad": 50,
-    "ciudad": "Pujili",
-    "profesion": "Estudiante"
+    "nombre": "Luis Maigua",  # Nombre de la persona
+    "edad": 50,               # Edad de la persona
+    "ciudad": "Pujili",        # Ciudad actual de residencia
+    "profesion": "Jubilado"     # Profesión de la persona
 }
-informacion_personal["ciudad"] = "Guayaquil"
-informacion_personal["profesion"] = "Desarrollador de Software"
+# Acceder al valor asociado con la clave 'ciudad' y modificarlo
+# Aquí cambiamos la ciudad de "Pujili" a "Manta"
+informacion_personal["ciudad"] = "Manta"
+
+# Modificamos la profesión de la persona en la clave 'profesion'
+# Cambiamos de "Policia Nacional" a "Jubilado"
+informacion_personal["profesion"] = "Policia Nacional"
+
+# Verificar si la clave 'telefono' existe en el diccionario
+# Si no existe, la agregamos con un número de teléfono ficticio
 if "telefono" not in informacion_personal:
-       informacion_personal["telefono"] = "0998765432"
+    # Como 'telefono' no está en el diccionario, lo agregamos
+    informacion_personal["telefono"] = "0996813865"
+
+# Eliminar la clave 'edad' del diccionario
+# Consideramos que esta información no es necesaria
 if "edad" in informacion_personal:
     del informacion_personal["edad"]
+
+# Finalmente, imprimimos el diccionario actualizado
+# Este es el resultado después de todas las operaciones
 print(informacion_personal)
